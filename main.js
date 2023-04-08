2ff4d3e99f0025346d30d6d8c875611444071686
Labib-Ahammed <101618935+LABIB087@users.noreply.github.com>
2023-04-08 15:28:43 +0600

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ bar.addEventListener('click',()=>{
         menu.classList.add('hidden');
     }
 });
+
+//This is the code of project path
+
+const food_website = document.getElementById('food');
+
+const ecommerce_website = document.getElementById('ecommerce');
+
+const calculator_website = document.getElementById('calculator');
+
+
+food_website.addEventListener('click', function() {
+  location.assign("https://labib087.github.io/Restaurant/");
+});
+
+ecommerce_website.addEventListener('click', function() {
+  location.assign("https://labib087.github.io/Labib_Express/");
+
+});
+
+calculator_website.addEventListener('click',function(){
+  location.assign("https://labib087.github.io/calculator/")
+});
